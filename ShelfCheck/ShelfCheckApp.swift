9687e80801fabdea252aaa0ce39d6a5b55ca803a
Shashank Nayak <shashanknayak.74@gmail.com>
2026-09-11 01:21:06 +1000

    chore: initial project setup

---
+++ ShelfCheck/ShelfCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShelfCheckApp.swift
+//  ShelfCheck
+//
+//  Created by Shashank Nayak on 11/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShelfCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
